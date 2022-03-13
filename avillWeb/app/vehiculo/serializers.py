e6438a50eaeeb8e0128441c6dec9b6a9600ee50d
INGJEISSON <30418444+INGJEISSON@users.noreply.github.com>
2022-03-13 17:11:26 -0500

    changed serializer cars

--- avillWeb/app/vehiculo/serializers.py
+++ avillWeb/app/vehiculo/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import Vehiculo
 from ..account.serializers import AccountSerializer
+from ..servicios.serializers import ServiciosSerializer
 
 
 class VehiculoSerializer(serializers.ModelSerializer):
     persona = AccountSerializer(required=False)
     persona_id = serializers.IntegerField(required=False)
     placa= serializers.CharField(required=False)
-    servicio= serializers.IntegerField(required=False)
+    servicio= ServiciosSerializer(required=False)
 
 
     class Meta:
